e0b6959905ff343ed1b7bda343331b5f20ad8efe
曹祖鹏 <chinalibra@163.com>
2017-11-17 15:27:24 +0800

    new module

---
+++ uc/src/main/java/club/newtech/qbike/uc/UcApplication.java
@@ -0,0 +1,12 @@
+package club.newtech.qbike.uc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcApplication.class, args);
+	}
+}
---
+++ uc/src/test/java/club/newtech/qbike/uc/UcApplicationTests.java
@@ -0,0 +1,16 @@
+package club.newtech.qbike.uc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
