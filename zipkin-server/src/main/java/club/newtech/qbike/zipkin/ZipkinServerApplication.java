472c92d861abef07723f913d10a0a272f291c70e
曹祖鹏 <chinalibra@163.com>
2018-06-27 10:40:17 +0800

    升级Spring boot2 和 Spring cloud Finchley

--- zipkin-server/src/main/java/club/newtech/qbike/zipkin/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/club/newtech/qbike/zipkin/ZipkinServerApplication.java
@@ -2,7 +2,6 @@ package club.newtech.qbike.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
 
 /**
  * @author 曹祖鹏 OF506
@@ -10,7 +9,7 @@ import zipkin.server.EnableZipkinServer;
  * Date    2018-06-07
  */
 @SpringBootApplication
-@EnableZipkinServer
+//@EnableZipkinServer
 public class ZipkinServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
